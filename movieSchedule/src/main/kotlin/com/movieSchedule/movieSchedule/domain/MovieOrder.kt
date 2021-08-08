21422267b5db5cfda37657bd373d8238c906344e
Wagner Paulino <wagnersantos9778@gmail.com>
2021-08-08 20:10:34 -0300

    ajuste na entidade

--- movieSchedule/src/main/kotlin/com/movieSchedule/movieSchedule/domain/MovieOrder.kt
+++ movieSchedule/src/main/kotlin/com/movieSchedule/movieSchedule/domain/MovieOrder.kt
@@ -4,7 +4,9 @@ import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.Column
 import java.time.LocalDate
+import javax.persistence.Entity
 
+@Entity
 class MovieOrder(
     @Id @GeneratedValue var id: Long? = null,
     @Column var orderData: LocalDate? = null,
