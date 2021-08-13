a6b020750b8a8fd74d694a93630d49729f3b6b2f
Wagner Paulino <wagnersantos9778@gmail.com>
2021-08-12 22:23:41 -0300

    recebendo a message serializada

--- movieSchedule/src/main/kotlin/com/movieSchedule/movieSchedule/receiver/ReceiverMessage.kt
+++ movieSchedule/src/main/kotlin/com/movieSchedule/movieSchedule/receiver/ReceiverMessage.kt
@@ -1,11 +1,20 @@
 package com.movieSchedule.movieSchedule.receiver
 
 import org.springframework.stereotype.Component
+import org.springframework.beans.factory.annotation.Autowired
+import com.google.gson.Gson
+import com.movieSchedule.movieSchedule.domain.MovieOrder
+import com.movieSchedule.movieSchedule.repository.MovieOrderRepository
+import com.movieSchedule.movieSchedule.specification.MovieOrderSpecification
 
 @Component
-public class ReceiverMessage {
+public class ReceiverMessage(@Autowired private val movieOrderRepository: MovieOrderRepository) {
 
-    fun receiveMessage(message: String) {
-        print(message)
+    fun receiveMessage(movieOrder: String) {
+        val movieSpec = MovieOrderSpecification()
+        val gson = Gson()
+        val movie = gson.fromJson(movieOrder, MovieOrder().javaClass)
+        movieOrderRepository.findOne(movieSpec.findByMovieId(movie.movieId)).ifPresent{movieOrderSaved -> movie.id = movieOrderSaved.id}
+        movieOrderRepository.save(movie)
     }
 }
