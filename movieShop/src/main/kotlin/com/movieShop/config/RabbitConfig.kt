fece636f783e48930071bae76afc2a46951d35bd
WagnerPaulino <wagnersantos9778@gmail.com>
2023-08-20 00:04:41 -0300

    Adjustment in the config classes

--- movieShop/src/main/kotlin/com/movieShop/config/RabbitConfig.kt
+++ movieShop/src/main/kotlin/com/movieShop/config/RabbitConfig.kt
@@ -1,8 +1,5 @@
 package com.movieShop.config
 
-import org.springframework.context.annotation.Configuration
-
-@Configuration
 class RabbitConfig {
 
     val topicExchangeNameSchedule: String = "movie-schedule-exchange"
